ad7c23f283b1f7778e4cc740a7051cbcc91a4b1e
Thomas Dethorey <thomas.dethorey01@gmail.com>
2018-10-14 16:04:59 +0200

    ex 1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function bordureimage(){
+  document.getElementById("image1").style.border = "3px solid red";
+}
+function plusimage(){
+  document.getElementById("image1").style.border = null;
+  ;
+}
